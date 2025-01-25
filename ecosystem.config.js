2e241ffbb53e2b2e20b276a2ac81221e6b34680b
Alejo Sanchez <alejoh908@gmail.com>
2025-01-25 17:01:33 -0500

    Add sistem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,8 @@
 module.exports = {
     apps : [{
       name   : "Complex Bot",
-      script : "./index.js"
+      script : "./index.js",
+      max_memory_restart: "4G",  // Reinicia si supera 4 GB
+      node_args: "-max-old-space-size=4096"  // Límite de memoria de 4 GB
     }]
   }
